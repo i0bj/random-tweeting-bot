c3e04caa0f8ab00f367cfd1b52361a064ff4e8c8
Obehi Iredia <44153313+i0bj@users.noreply.github.com>
2019-02-22 02:14:51 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,34 +9,33 @@ var retweet = function() {
         result_type: 'recent',
         lang: 'en'
     }
-    // for more parametes, see: https://dev.twitter.com/rest/reference/get/search/tweets
+   
 
     Twitter.get('search/tweets', params, function(err, data) {
-      // if there no errors
+      
         if (!err) {
-          // grab ID of tweet to retweet
+          
             var retweetId = data.statuses[0].id_str;
-            // Tell TWITTER to retweet
+            
             Twitter.post('statuses/retweet/:id', {
                 id: retweetId
             }, function(err, response) {
                 if (response) {
                     console.log('Retweeted!!!');
                 }
-                // if there was an error while tweeting
+                
                 if (err) {
                     console.log('Something went wrong while RETWEETING... Duplication maybe...');
                 }
             });
         }
-        // if unable to Search a tweet
+        
         else {
           console.log('Something went wrong while SEARCHING...');
         }
     });
 }
 
-//retweet as soon as program is running...
 retweet();
-// retweet in every 50 minutes
+
 setInterval(retweet, 3000000);
